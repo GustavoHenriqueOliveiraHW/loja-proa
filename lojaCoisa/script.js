6d3c94d6340098cc272c5734b7dba7e7109bfc2e
GustavoHenriqueOliveiraHW <gustavo.oliveira4@aluno.ifsp.edu.br>
2026-06-16 14:57:13 -0300

    son

--- lojaCoisa/script.js
+++ lojaCoisa/script.js
@@ -3,6 +3,7 @@ constructor(nome, preco, descricao, imagepath){
     this.nome = nome;
     this.preco = preco;
     this.imagepath = imagepath;
-    this.descricao = descricao
+    this.descricao = descricao;
 }
 }
+const salgadinho = new produto("cheetos", 1.99, "Salgadinho bem bonito", "cheetos.webp");

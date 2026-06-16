ef9985bb3ae0cae58c8abf9da1fcc1f686885d5e
GustavoHenriqueOliveiraHW <gustavo.oliveira4@aluno.ifsp.edu.br>
2026-06-16 14:52:06 -0300

    comita

---
+++ lojaCoisa/script.js
@@ -0,0 +1,8 @@
+class produto{
+constructor(nome, preco, descricao, imagepath){
+    this.nome = nome;
+    this.preco = preco;
+    this.imagepath = imagepath;
+    this.descricao = descricao
+}
+}

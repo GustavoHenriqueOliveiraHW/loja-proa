9eb69ea0276d7cea480acd6bcf3cb407e207e150
GustavoHenriqueOliveiraHW <gustavo.oliveira4@aluno.ifsp.edu.br>
2026-06-30 13:43:28 -0300

    Delete lojaCoisa directory

--- lojaCoisa/script.js
+++
@@ -1,9 +0,0 @@
-class produto{
-constructor(nome, preco, descricao, imagepath){
-    this.nome = nome;
-    this.preco = preco;
-    this.imagepath = imagepath;
-    this.descricao = descricao;
-}
-}
-const salgadinho = new produto("cheetos", 1.99, "Salgadinho bem bonito", "cheetos.webp");

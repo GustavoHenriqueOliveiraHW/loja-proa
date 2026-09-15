06435b9e42abb258af16088b611a20c8a0b44c02
GustavoHenriqueOliveiraHW <gustavo.oliveira4@aluno.ifsp.edu.br>
2026-09-15 12:23:18 -0300

    Add files via upload

--- lojaCoisa/scripts/cadastro.js
+++ lojaCoisa/scripts/cadastro.js
@@ -1,26 +1,55 @@
-
-// turmas = ["1°Info","2°Info","Meca"];
-document.getElementById("form").addEventListener("submit",function(e){
-    e.preventDefault();
     const nome = document.getElementById("nome");
 const idade = document.getElementById("idade");
 const turma = document.getElementById("turma")
 const senha = document.getElementById("senha")
-const confirmar_senha = document.getElementById("validar-senha")
+const confirmarSenha = document.getElementById("validar-senha")
+const form = document.getElementById("form");
 
+class User{
+    constructor(nome, idade, turma, senha){
+        this.nome = nome;
+        this.idade = idade;
+        this.turma = turma;
+        this.senha = senha;
+    }
+}
+
+const message = document.createElement("p");
+message.id = "message";
+document.querySelector("main").append(message);
+
+// turmas = ["1°Info","2°Info","Meca"];
+form.addEventListener("submit",function(e){
+    e.preventDefault();
    const hasNumberRegex = /\d/;
     if(hasNumberRegex.test(nome.value)){
-        const h1 = document.createElement("p")
-        h1.textContent = "Nome não pode conter números";
-        document.querySelector("main").append(h1);
-        return;
+        message.textContent = "Nome não pode conter números";
+        return; 
     }
     if(nome.value === ""){
-        const h1 = document.createElement("p")
-        h1.textContent = "Nome não pode ser vazio";
-        document.querySelector("main").append(h1);
+        message.textContent = "Nome não pode ser vazio";
         return;
-    };
+    }
+    if(idade.value <= 0){
+        message.textContent = "Idade tem de ser positiva";
+        return;
+    }
+    if(turma.value === null){
+        message.textContent = "Selecione uma turma";
+        return;
+    }
+    if(senha.value === ""){
+        message.textContent = "Senha não pode ser vazia";
+    }
+    if(senha.value !== confirmarSenha.value){
+       message.textContent = "Confirmar senha tem de ser igual à senha"; 
+    }
+    else{
+        message.textContent = "";
+        const user = new User(nome.value, idade.value, turma.value, senha.value);
+        localStorage.setItem("user", JSON.stringify(user))
+    }
+    
 })
 
 
